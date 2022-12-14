d4dbe19bd01e1b0aa6f5966833b69569b4fd3aee
vinayak-naik <vinayaknaik2021@gmail.com>
2022-12-14 10:56:21 +0530

    articles 2

--- public/static/articles/json/index.ts
+++ public/static/articles/json/index.ts
@@ -3,8 +3,8 @@ import chapter2 from './chapter-2.json'
 import chapter3 from './chapter-3.json'
 import chapter4 from './chapter-4.json'
 import chapter5 from './chapter-5.json'
-import chapter6 from './chapter-6.json'
+// import chapter6 from './chapter-6.json'
 
 export const articlesJson=[
-    chapter1,chapter2,chapter3,chapter4,chapter5,chapter6
+    chapter1,chapter2,chapter3,chapter4,chapter5
 ]
